3e73d4ff92b307d1caf741251b119100eeaa15ed
Chunglnt <chunglnt@vnpost.vn>
2025-05-28 10:33:30 +0700

    git push

--- lib/axios.ts
+++ lib/axios.ts
@@ -19,6 +19,12 @@ api.interceptors.request.use(
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
         }
+        // Dynamically set baseURL for GitHub API requests
+        if (config.url?.startsWith("/repos/")) {
+            config.baseURL = "https://api.github.com"; // Set GitHub API base URL
+            config.headers.Accept = "application/vnd.github.v3+json"; // GitHub API version
+            config.headers.Authorization = `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`
+        }
 
         return config
     },
